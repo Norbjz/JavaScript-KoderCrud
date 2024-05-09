e0647126851bb34a0428ecb5f1bc45b4827abb46
Norbjz <norberto.j90@gmail.com>
2024-05-08 20:06:37 -0600

    cambio ruta index

--- js/login.js
+++ js/login.js
@@ -18,5 +18,5 @@ logInBtn.addEventListener("click", (event) => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
   localStorage.setItem("token", token);
-  window.open("../views/index.html", "_self");
+  window.open("../index.html", "_self");
 });
